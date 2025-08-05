981f06298acb781636c4ab03b72462e220c446db
ruanmuller20 <ruanmuller20@gmail.com>
2025-08-05 09:50:23 -0300

    integração front/back do  login de credenciado, entregador e cliente

--- Foodzap-Credenciados/login/loginCredenciado.js
+++ Foodzap-Credenciados/login/loginCredenciado.js
@@ -11,6 +11,51 @@ menuToggle.addEventListener("click", function () {
 const cpfUsuario = document.getElementById("cpfUsuario");
 const senhaUsuario = document.getElementById("senhaUsuario");
 
-document.getElementById("btnCadastro").addEventListener("click", function () {
-   
-})
+document.getElementById('btnLogin').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const cpfValue = cpfUsuario.value.trim();
+    const senhaValue = senhaUsuario.value.trim();
+
+
+    if(cpfValue === '' || senhaValue === ''){
+        alert('Por favor, preencha todos os campos!');
+      
+    } else {
+      fetch('http://localhost:3333/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cpfCnpj: cpfValue,
+          password: senhaValue
+        })
+      })
+      .then(response => {
+        console.log('Resposta da API:', response);
+        return response.json();
+      })
+      .then(data => {
+        console.log('Dados recebidos:', data);
+        if (data.error){
+          throw new Error(data.error);
+        }
+
+        localStorage.setItem('token', data.token);
+      
+        alert('Login realizado com sucesso!');
+        setTimeout(() => {
+          window.location.href = '../../paginaInicial.html';
+        }, 2600);
+
+
+      })
+      .catch(error => {
+        console.log('Erro na requisição', error);
+        alert('Erro ao fazer login: ' + error.message);
+      });
+
+
+    };
+});
